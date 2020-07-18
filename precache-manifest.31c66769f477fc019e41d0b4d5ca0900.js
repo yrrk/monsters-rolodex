221e902545d15026bbbb151d7cb36545dab49514
yaksh thakkar <kinyakrr@gmail.com>
2020-07-18 16:33:33 +0530

    Updates

---
+++ precache-manifest.31c66769f477fc019e41d0b4d5ca0900.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e35ccd49c9116e6e062d441eddcd7d2f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "92bd0b3006d99bb1c59e",
+    "url": "/monsters-rolodex/static/css/main.0509a5d0.chunk.css"
+  },
+  {
+    "revision": "ebbbd6101675d0285ec8",
+    "url": "/monsters-rolodex/static/js/2.f24ca167.chunk.js"
+  },
+  {
+    "revision": "e46fc7d39878ee64e7b083a156855138",
+    "url": "/monsters-rolodex/static/js/2.f24ca167.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92bd0b3006d99bb1c59e",
+    "url": "/monsters-rolodex/static/js/main.bc6e7242.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
